ce041193f1ff20a938570df3681544b85288b422
Delta <riku@kawaii.sh>
2020-09-06 15:54:37 -0500

    Added recents

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <Navbar></Navbar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<style lang="scss">
+.centered {
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+  created () {
+    this.$vuetify.theme.dark = true
+  },
+}
+</script>
